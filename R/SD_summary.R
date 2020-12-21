68e7e2ba552666925e5f0708f964723b7d7f400b
Nexller <songbaozou@126.com>
2020-12-21 09:58:26 +0800

    new

---
+++ R/SD_summary.R
@@ -0,0 +1,26 @@
+#' 标准差汇总
+#' 该程序用于计算数据框的平均值和标准差SD
+#' @param data 数据框
+#' @param varname 目标数据列
+#' @param groupnames 分组
+#' @param digtal 结果的小数点
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' SD_summary(iris,varname = "Petal.Width", groupnames = "Species", digtal = 2)
+
+SD_summary <- function(data, varname, groupnames, digtal){
+  require(dplyr)
+  require(plyr)
+  summary_func <- function(x, col){
+    c(mean = round(mean(x[[col]], na.rm=TRUE), digits = digtal),
+      sd = round(sd(x[[col]], na.rm=TRUE), digits = digtal))
+  }
+  data_sum<-ddply(data, groupnames, .fun=summary_func,
+                  varname)
+  data_sum <- rename(data_sum, c("mean" = varname))
+  return(data_sum)
+}
+
---
+++ R/multi.ttest.R
@@ -0,0 +1,23 @@
+#' 多重t-test计算
+#' 这个函数用于计算多个变量，两两之间的t-test
+#' @param mat dataframe
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+multi.ttest <- function(mat, ...) {
+  mat <- as.matrix(mat)
+  n <- ncol(mat)
+  p.mat<- matrix(NA, n, n)
+  diag(p.mat) <- 1
+  for (i in 1:(n - 1)) {
+    for (j in (i + 1):n) {
+      test <- t.test(paired = T,mat[, i], mat[, j], ...)
+      p.mat[i, j] <- p.mat[j, i] <- test$p.value
+    }
+  }
+  colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
+  signif(p.mat,4)
+}
